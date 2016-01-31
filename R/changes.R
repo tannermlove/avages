85485a466ab1878d06000516a48d1dac039ff477
Tanner Matthew Love <tml3@williams.edu>
2016-01-30 23:40:54 -0500

    added experimental function to test whether it will install on another computer

---
+++ R/changes.R
@@ -0,0 +1,5 @@
+graphy <- function() {
+  ages1 <- c(avg_age(2004), avg_age(2005), avg_age(2006), avg_age(2007), avg_age(2008), avg_age(2009), avg_age(2010), avg_age(2011), avg_age(2012), avg_age(2013), avg_age(2014), avg_age(2015));
+  qplot(seq(2004:2014), ages1)
+}
+
